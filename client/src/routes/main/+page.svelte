5c26e6bf69ccbaffd1658044f1d4099e055088bb
Freckles <freckles@openselves.org>
2026-04-19 12:38:49 +0200

    client: refetch user from api on main page everytime sync is back online

--- client/src/routes/main/+page.svelte
+++ client/src/routes/main/+page.svelte
@@ -3,6 +3,7 @@
 	import { resolve } from "$app/paths";
 	import { MenuItem } from "$lib";
 	import { call, handleLogout } from "$lib/api.svelte";
+	import { appNetworkStatus } from "$lib/app-network-status.svelte";
 	import AppPage from "$lib/components/AppPage.svelte";
 	import LanguageSwitcher from "$lib/components/forms/LanguageSwitcher.svelte";
 	import LogoutIcon from "$lib/components/icons/LogoutIcon.svelte";
@@ -10,40 +11,39 @@
 	import { Block, BlockTitle, Button, Preloader } from "konsta/svelte";
 	import { onMount } from "svelte";
 
+	let storage: Storage | undefined = $state();
+	let storageKey: string | undefined = $derived(storage?.getKey());
 	let user:
 		| {
 				id: string;
 				email: string;
 		  }
-		| undefined = undefined;
-	let offline = false;
-	let storageKey: string | undefined = undefined;
+		| undefined = $state();
 
-	const load = (async () => {
-		const storage = await Storage.getStorage();
+	onMount(async () => {
+		storage = await Storage.getStorage();
 		if (storage.isOffline()) {
 			await goto(resolve("/"));
 		}
-	})();
-	onMount(async () => {
-		await load;
-		const storage = await Storage.getStorage();
-		if (storage.isOffline() || !navigator.onLine) {
-			offline = true;
-			storageKey = storage.getKey();
-			return;
-		}
+	});
 
-		const response = await call(`/user/${storage.getKey()}`);
-		if (response) {
-			if (!("id" in response && "email" in response)) {
-				throw new Error("Bad server response");
+	$effect(() => {
+		let online = appNetworkStatus.syncWorkerOnline;
+		let userId = storageKey;
+		(async () => {
+			if (online && userId) {
+				const response = await call(`/user/${userId}`);
+				if (response) {
+					if (!("id" in response && "email" in response)) {
+						throw new Error("Bad server response");
+					}
+					user = {
+						id: `${response.id}`,
+						email: `${response.email}`,
+					};
+				}
 			}
-			user = {
-				id: `${response.id}`,
-				email: `${response.email}`,
-			};
-		}
+		})();
 	});
 
 	const logoutButtonOnclick = async () => {
@@ -68,9 +68,9 @@
 
 	<BlockTitle medium>Status</BlockTitle>
 	<Block strong>
-		{#if user}
+		{#if appNetworkStatus.syncWorkerOnline && user}
 			<p>{t("You are logged in as user #{user.id}, {user.email}", user.id, user.email)}</p>
-		{:else if offline && storageKey}
+		{:else if !appNetworkStatus.syncWorkerOnline && storageKey}
 			<p>{t("Offline - #{user.id}", storageKey)}</p>
 		{:else}
 			<Preloader />
