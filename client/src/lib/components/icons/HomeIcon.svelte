ac662c9ca64c6a44d68677d0da61b6fff2ba1570
Freckles <freckles@openselves.org>
2026-06-03 13:08:23 +0200

    client: make sure lint fails after build for unused icon auto imports, remove unused icon

--- client/src/lib/components/icons/HomeIcon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import OSIcon from "$lib/components/OSIcon.svelte";
-	import type { OSIconProps } from "$lib/components/os-icon";
-
-	let props: Omit<OSIconProps, "material" | "ios"> = $props();
-</script>
-
-<OSIcon material={IconIcRoundHome} ios={IconF7HouseFill} {...props} />
